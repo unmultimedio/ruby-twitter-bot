38c0cfc4535c38c9e39fd66ee0d19cb8d32ee690
Julián R Figueroa <julian@zentrifuga.com>
2017-05-01 18:07:24 -0500

    Added reply, auto_reply, and list of books

--- bot.rb
+++ bot.rb
@@ -6,8 +6,11 @@ require 'Twitter'
 puts '****************************************'
 puts 'Welcome, type `bot_help` for help.'
 puts '****************************************'
+
+# Global variables
 @client = nil
 
+# Loads an account based on the secrets YAML file
 def load(user)
   accounts = YAML.load_file('secrets.yml')
   user_account = accounts[user]
@@ -22,11 +25,16 @@ def load(user)
   true
 end
 
+def search(query, amount: 50, options: {})
+  @client.search(query, options).take(amount)
+end
+
 def fav(query, options: {}, amount: 50, skip: true, one_per_query: true)
   return if query.empty?
   faved = 0
   faved_users = []
-  @client.search(query, options).take(amount).each do |tweet|
+  tweets = search(query, amount: amount, options: options)
+  tweets.each do |tweet|
     if skip && @followers.include?(tweet.user.id)
       puts "Skipped(@#{tweet.user.screen_name}): #{tweet.text}"
       next
@@ -44,8 +52,50 @@ def fav(query, options: {}, amount: 50, skip: true, one_per_query: true)
   faved
 end
 
-def search(query, amount: 50, options: {})
-  @client.search(query, options).take(amount)
+def reply(query, options: {}, amount: 50, skip: true, one_per_query: true)
+  return if query.empty?
+  replied = 0
+  replied_users = []
+  tweets = search(query, amount: amount, options: options)
+  tweets.each do |tweet|
+    if skip && @followers.include?(tweet.user.id)
+      puts "Skipped(@#{tweet.user.screen_name}): #{tweet.text}"
+      next
+    end
+    if one_per_query && replied_users.include?(tweet.user.id)
+      puts "Skipped(@#{tweet.user.screen_name}): #{tweet.text}"
+      next
+    end
+    @client.update("@#{tweet.user.screen_name} ¡Hey!",
+                   in_reply_to_status_id: tweet.id)
+    replied_users << tweet.user.id
+    replied += 1
+    puts "Replied(@#{tweet.user.screen_name}): #{tweet.text}"
+  end
+  puts "Replied #{replied} tweets."
+end
+
+def reply_book_loop(query, options: {})
+  return if query.empty?
+  replied_tweets = []
+  total_replied = 0
+  loop do
+    replied = 0
+    puts "#{Time.new}: Started replying..."
+    tweets = search(query, options: options)
+    tweets.each do |tweet|
+      next if replied_tweets.include?(tweet.id)
+      @client.update("@#{tweet.user.screen_name} \"#{random_book}\" @BogotaRuby #RubyDev #Filbo2017",
+                     in_reply_to_status_id: tweet.id)
+      replied_tweets << tweet.id
+      replied += 1
+      puts "Replied(@#{tweet.user.screen_name}): #{tweet.text}"
+    end
+    total_replied += replied
+    puts "New #{replied} replies, out of #{total_replied} total replies."
+    puts 'Sleeping...'
+    sleep(5) # Wait for 5 seconds until replying again
+  end
 end
 
 def common_options(lang: true, geocode: true)
@@ -53,6 +103,27 @@ def common_options(lang: true, geocode: true)
   opts[:lang] = 'es' if lang # Spanish only tweets
   opts[:geocode] = '4.753,-74.113,1150km' if geocode # Colombia (and a bit more around)
   opts
+end
+
+def random_book
+  [
+    'Business Model Generation (Alexander Osterwalder)',
+    'Digital Wars (Charles Arthur)',
+    'El diario de Anna Frank',
+    'Elon Musk: Tesla, SpaceX, and the Quest for a Fantastic Future (Ashlee Vance)',
+    'Getting Real (37signals)',
+    'How to win friends and influence people (Dale Carnegie)',
+    'Las cuatro vidas de Steve Jobs (Daniel Ichbiah)',
+    'Los Innovadores (Walter Isaacson)',
+    'Rework (Jason Fried)',
+    'Rojo Sombra (Gabriela A. Arciniegas)',
+    'Slicing Pie: Fund Your Company Without Funds (Mike Moyer)',
+    'The 4-Hour Workweek (Timothy Ferriss)',
+    'The Founder\'s Dilemmas (Noam Wasserman)',
+    'The Great Design (Stephen Hawking, Leonard Mlodinow)',
+    'The Lean Startup (Eric Ries)',
+    'Zero to One (Peter Thiel)'
+  ].sample
 end
 
 def bot_help
